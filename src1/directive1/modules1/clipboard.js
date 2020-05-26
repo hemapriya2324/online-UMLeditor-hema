2818e1bf099620cdbd602e285acb71f80acb215c
hemapriya2324 <46390689+hemapriya2324@users.noreply.github.com>
2020-05-26 14:12:31 +0530

    Create clipboard.js

---
+++ src1/directive1/modules1/clipboard.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import Clipboard from 'clipboard'
+import $ from 'jquery' // コンパイルを通すためだけの書き方
+interface jQuery {}
+
+// https://clipboardjs.com/
+export default Vue.directive('clipboard', {
+  inserted(element: any) {
+    $(element)
+      .css({
+        cursor: 'pointer'
+      })
+      .data('toggle', 'tooltip')
+      .prop('title', 'Copied')
+      .tooltip({
+        trigger: 'manual'
+      })
+      .on('click', function() {
+        const $this: jQuery = $(this)
+        $this.tooltip('show')
+        setTimeout(() => {
+          $this.tooltip('hide')
+        }, 500)
+      })
+    new Clipboard(element, {
+      target(): any {
+        return element
+      }
+    })
+  }
+})
