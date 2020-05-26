aa1fe097e5db27d0bceae145d53ba6f8067d4237
hemapriya2324 <46390689+hemapriya2324@users.noreply.github.com>
2020-05-26 14:13:33 +0530

    Create index.js

---
+++ src1/directive1/index.js
@@ -0,0 +1,11 @@
+/* @flow */
+
+import Vue from 'vue'
+
+import VeeValidate from 'vee-validate'
+Vue.use(VeeValidate)
+
+import Clipboard from './modules/clipboard'
+Vue.use(Clipboard)
+
+export default {}
