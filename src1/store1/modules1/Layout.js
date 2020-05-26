a518299450925f740aed00bb7ba519714357e7c2
hemapriya2324 <46390689+hemapriya2324@users.noreply.github.com>
2020-05-26 14:23:36 +0530

    Create Layout.js

---
+++ src1/store1/modules1/Layout.js
@@ -0,0 +1,71 @@
+/* @flow */
+
+const state: any = {
+  colSize: {
+    history: 2,
+    editor: 4,
+    cheatSheet: 0,
+    uml: 6
+  }
+}
+
+const mutations: any = {
+  setColSize(state: any, colSize: any) {
+    if (colSize.history || colSize.history === 0) {
+      state.colSize.history = colSize.history
+    }
+    if (colSize.editor || colSize.editor === 0) {
+      state.colSize.editor = colSize.editor
+    }
+    if (colSize.cheatSheet || colSize.cheatSheet === 0) {
+      state.colSize.cheatSheet = colSize.cheatSheet
+    }
+    if (colSize.uml || colSize.uml === 0) {
+      state.colSize.uml = colSize.uml
+    }
+  }
+}
+
+const getters: any = {
+  isCloseHistory({ colSize }: any): boolean {
+    return colSize.history === 0
+  }
+}
+
+const actions: any = {
+  setColSize(context: any, colSize: any) {
+    context.commit('setColSize', colSize)
+  },
+  setEditColSize(context: any) {
+    context.commit('setColSize', {
+      history: 0,
+      editor: 5,
+      cheatSheet: 0,
+      uml: 7
+    })
+  },
+  setCheatSheetColSize(context: any) {
+    context.commit('setColSize', {
+      history: 0,
+      editor: 4,
+      cheatSheet: 3,
+      uml: 5
+    })
+  },
+  resetColSize(context: any) {
+    context.commit('setColSize', {
+      history: 2,
+      editor: 4,
+      cheatSheet: 0,
+      uml: 6
+    })
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  getters,
+  actions
+}
